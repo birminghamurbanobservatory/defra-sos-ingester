37cae3894455d1f60a04eb48beee6b2cc5c3808d
SiBell <SiBellUK@gmail.com>
2020-06-30 19:29:57 +0100

    limit added

--- src/ingest/sos.service.ts
+++ src/ingest/sos.service.ts
@@ -14,12 +14,13 @@ export async function getTimeseries(timeseriesId: number, startDate: Date, endDa
 
   const startIso = startDate.toISOString();
   const endIso = endDate.toISOString();
+  const limit = 1000; // worth setting this because if you don't the server will try to get you everything which can take a very long time, so it's best to limit it. If it failed to get everything the first ime then the next time the CronJob runs it can just pick up where it left off.
   
   try {
     
     // Need to use querystring.stringify as Netatmo expects body to be x-www-form-urlencoded formatted
     response = await axios.get(
-      `https://uk-air.defra.gov.uk/sos-ukair/api/v1/timeseries/${timeseriesId}/getData?timespan=${startIso}/${endIso}`,
+      `https://uk-air.defra.gov.uk/sos-ukair/api/v1/timeseries/${timeseriesId}/getData?timespan=${startIso}/${endIso}&limit=${limit}`
     );
 
   } catch (err) {
